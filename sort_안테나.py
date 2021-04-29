297b6f369c27c191dfcf64b482d8767a4e14aab0
Yuheun Kim <49318161+yuheunk@users.noreply.github.com>
2021-04-30 01:54:14 +0900

    210430-sort

---
+++ sort_안테나.py
@@ -0,0 +1,9 @@
+# 일직선상에 여러 채의 집이 위치함.
+# 이 중 하나에 안테나를 설치하는데 모든 집까지의 거리가 최소가 되도록 설치한다.
+# output: 설치할 집의 위치(여러 개면 가장 작은 값)
+
+n = int(input('number of houses'))
+houses = list(map(int, input('Location of houses').split()))
+
+houses.sort()
+print(houses[len(houses)//2-1])
