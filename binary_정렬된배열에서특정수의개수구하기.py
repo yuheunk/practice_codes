0141792e7c7398b03c9cacb4d535cb448f18880a
Yuheun Kim <49318161+yuheunk@users.noreply.github.com>
2021-05-08 00:22:49 +0900

    Update binary_정렬된배열에서특정수의개수구하기.py

--- binary_정렬된배열에서특정수의개수구하기.py
+++ binary_정렬된배열에서특정수의개수구하기.py
@@ -1,7 +1,7 @@
 n, x = map(int, input().split())
 array = list(map(int, input().split()))
 
-def binary_search(array, target, start, end):
+def lft_search(array, target, start, end):
     if len(array)==1:
         if target in array:
             return array.index(target)
@@ -15,22 +15,41 @@ def binary_search(array, target, start, end):
             return mid
         
         elif array[mid] == target and array[mid-1] >= target:
-            return binary_search(array, target, start, mid-1)
+            return lft_search(array, target, start, mid-1)
         
         elif array[mid] > target:
-            return binary_search(array, target, start, mid-1)
+            return lft_search(array, target, start, mid-1)
         else:
-            return binary_search(array, target, mid+1, end)
+            return lft_search(array, target, mid+1, end)
     return -1
 
-count = 0
-result = binary_search(array, x, 0, n-1)
+def rgt_search(array, target, start, end):
+    if len(array)==1:
+        if target in array:
+            return 1
+        else:
+            return -1
 
-if result != -1:
-    for a in array[result:]:
-        if a==x:
-            count+=1
+    while start <= end:
+        mid = (start+end)//2
+
+        if array[mid] == target and array[mid+1] > target:
+            return mid
+        
+        elif array[mid] == target and array[mid+1] <= target:
+            return rgt_search(array, target, mid+1, end)
+        
+        elif array[mid] > target:
+            return rgt_search(array, target, start, mid-1)
+        else:
+            return rgt_search(array, target, mid+1, end)
+    return -1
+
+
+start = lft_search(array, x, 0, n-1)  # target 숫자가 시작하는 인덱스
+end = rgt_search(array, x, 0, n-1)  # target 숫자가 끝나는 인덱스
+
+if start != -1 and end != -1:
+    print(end-start+1)
 else:
-    count = -1
-
-print(count)
+    print(start)
